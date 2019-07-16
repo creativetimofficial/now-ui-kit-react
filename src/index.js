f042b2e19f70cee6fbb2e8770c306289e4745d2b
Manu <emanuelnazare@yahoo.ro>
2019-07-16 09:38:55 +0300

    - started to work on makeing the theme start

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Index from "views/Index.js";
+import NucleoIcons from "views/NucleoIcons.js";
+import LoginPage from "views/examples/LoginPage.js";
+import LandingPage from "views/examples/LandingPage.js";
+import ProfilePage from "views/examples/ProfilePage.js";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Switch>
+        <Route path="/index" render={props => <Index {...props} />} />
+        <Route
+          path="/nucleo-icons"
+          render={props => <NucleoIcons {...props} />}
+        />
+        <Route
+          path="/landing-page"
+          render={props => <LandingPage {...props} />}
+        />
+        <Route
+          path="/profile-page"
+          render={props => <ProfilePage {...props} />}
+        />
+        <Route path="/login-page" render={props => <LoginPage {...props} />} />
+        <Redirect to="/index" />
+        <Redirect from="/" to="/index" />
+      </Switch>
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
