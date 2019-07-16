6f79dee9c9d90fbca107df07e89c93e705ab61ac
Manu <emanuelnazare@yahoo.ro>
2019-07-16 13:37:41 +0300

    - working on BasicElements - Added styles for nouislider

--- src/views/index-sections/BasicElements.js
+++ src/views/index-sections/BasicElements.js
@@ -1,6 +1,8 @@
 import React from "react";
 // react plugin used to create switch buttons
 import Switch from "react-bootstrap-switch";
+// plugin that creates slider
+import Slider from "nouislider";
 
 // reactstrap components
 import {
@@ -21,6 +23,30 @@ import {
 function BasicElements() {
   const [leftFocus, setLeftFocus] = React.useState(false);
   const [rightFocus, setRightFocus] = React.useState(false);
+  React.useEffect(() => {
+    if (
+      !document
+        .getElementById("sliderRegular")
+        .classList.contains("noUi-target")
+    ) {
+      Slider.create(document.getElementById("sliderRegular"), {
+        start: [37.5],
+        connect: [true, false],
+        step: 0.5,
+        range: { min: 0, max: 100 }
+      });
+    }
+    if (
+      !document.getElementById("sliderDouble").classList.contains("noUi-target")
+    ) {
+      Slider.create(document.getElementById("sliderDouble"), {
+        start: [20, 80],
+        connect: [false, true, false],
+        step: 1,
+        range: { min: 0, max: 100 }
+      });
+    }
+  });
   return (
     <>
       <div className="section section-basic" id="basic-elements">
