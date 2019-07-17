d9cc8bbf89eb0accf728c0b9a903514e33b90d27
Manu <emanuelnazare@yahoo.ro>
2019-07-17 16:49:48 +0300

    Updates

---
+++ precache-manifest.b7c0c2508fe85180ba0be4b23915b34b.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc279b5c35416dee612974a56d47cfeb",
+    "url": "/now-ui-kit-react/index.html"
+  },
+  {
+    "revision": "3910c4b6ea183bb9b83c",
+    "url": "/now-ui-kit-react/static/css/main.611e5dd2.chunk.css"
+  },
+  {
+    "revision": "68de8b25c642b8d9088e",
+    "url": "/now-ui-kit-react/static/js/2.032e8edf.chunk.js"
+  },
+  {
+    "revision": "3910c4b6ea183bb9b83c",
+    "url": "/now-ui-kit-react/static/js/main.1c5d1385.chunk.js"
+  },
+  {
+    "revision": "71731f0fbde259263994",
+    "url": "/now-ui-kit-react/static/js/runtime~main.dcd83035.js"
+  },
+  {
+    "revision": "7257a1a94be550133f3233fd792ee724",
+    "url": "/now-ui-kit-react/static/media/avatar.7257a1a9.jpg"
+  },
+  {
+    "revision": "6b1983a26a32647c4832456dc9f5dcd3",
+    "url": "/now-ui-kit-react/static/media/bg1.6b1983a2.jpg"
+  },
+  {
+    "revision": "e643a6cf77b4635dda856f10fa9b7db4",
+    "url": "/now-ui-kit-react/static/media/bg11.e643a6cf.jpg"
+  },
+  {
+    "revision": "f807b37b1736e7709918ea35ef80ca7d",
+    "url": "/now-ui-kit-react/static/media/bg3.f807b37b.jpg"
+  },
+  {
+    "revision": "4bff5d45104e2e6c59b1825b54864d58",
+    "url": "/now-ui-kit-react/static/media/bg4.4bff5d45.jpg"
+  },
+  {
+    "revision": "056eb0586182a8d28273ad11c028e912",
+    "url": "/now-ui-kit-react/static/media/bg5.056eb058.jpg"
+  },
+  {
+    "revision": "d7d1aaf71d275992031bd75cdb6ec65c",
+    "url": "/now-ui-kit-react/static/media/bg6.d7d1aaf7.jpg"
+  },
+  {
+    "revision": "607a4a36d058e55773625ba23518e291",
+    "url": "/now-ui-kit-react/static/media/bg7.607a4a36.jpg"
+  },
+  {
+    "revision": "6737ce3b2577a1c3977e4d479511f642",
+    "url": "/now-ui-kit-react/static/media/bg8.6737ce3b.jpg"
+  },
+  {
+    "revision": "f7cb2d390498c2969b2b1050f3e024ce",
+    "url": "/now-ui-kit-react/static/media/creative-tim-white-slim2.f7cb2d39.png"
+  },
+  {
+    "revision": "089c4f4e55f158a69d0364c6b6339f37",
+    "url": "/now-ui-kit-react/static/media/eva.089c4f4e.jpg"
+  },
+  {
+    "revision": "ffcc99c42164b65da7af6307b66379cf",
+    "url": "/now-ui-kit-react/static/media/header.ffcc99c4.jpg"
+  },
+  {
+    "revision": "f06da25300d73a4d75c14d35471289d7",
+    "url": "/now-ui-kit-react/static/media/hero-image-1.f06da253.png"
+  },
+  {
+    "revision": "67a3fde9ab2e23a760ee9c98b2a4cc3e",
+    "url": "/now-ui-kit-react/static/media/hero-image-2.67a3fde9.png"
+  },
+  {
+    "revision": "62d2649b005f22aef38142952efc9ce5",
+    "url": "/now-ui-kit-react/static/media/hero-image-3.62d2649b.png"
+  },
+  {
+    "revision": "300494e9857b8d865ead79a62b09dd88",
+    "url": "/now-ui-kit-react/static/media/invision-white-slim.300494e9.png"
+  },
+  {
+    "revision": "138381e51c42397348a82b448b5fb61b",
+    "url": "/now-ui-kit-react/static/media/julie.138381e5.jpg"
+  },
+  {
+    "revision": "280baa1d29c6924f75b58478a04804d1",
+    "url": "/now-ui-kit-react/static/media/landing.280baa1d.jpg"
+  },
+  {
+    "revision": "b21019516fd574253e2354080dc1dd00",
+    "url": "/now-ui-kit-react/static/media/login.b2101951.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/now-ui-kit-react/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/now-ui-kit-react/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/now-ui-kit-react/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/now-ui-kit-react/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "069aba2640b0e99abe36914773e27ff9",
+    "url": "/now-ui-kit-react/static/media/profile.069aba26.jpg"
+  },
+  {
+    "revision": "f217f346c88e5fb2720ca7c28827ff72",
+    "url": "/now-ui-kit-react/static/media/ryan.f217f346.jpg"
+  }
+]);
