ff1d362c18aa1053d6d7531a21bdedaa7fee365f
Manu <emanuelnazare@yahoo.ro>
2019-07-18 09:59:08 +0300

    - small bug with the nueclo icons styles

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import "assets/css/bootstrap.min.css";
 import "assets/scss/now-ui-kit.scss";
 import "assets/demo/demo.css";
+import "assets/demo/nucleo-icons-page-styles.css";
 // pages for this kit
 import Index from "views/Index.js";
 import NucleoIcons from "views/NucleoIcons.js";
