a68f0347070ea6d3c480333452d30e1487418c27
Manu <emanuelnazare@yahoo.ro>
2019-07-19 11:37:59 +0300

    - collapse is done

--- src/views/docs/Sections/restyled-components/Collapse.js
+++ src/views/docs/Sections/restyled-components/Collapse.js
@@ -17,8 +17,6 @@ const codeExample = `import React from "react";
 
 import { UncontrolledCollapse, Button, CardBody, Card } from "reactstrap";
 
-const codeExample =
-
 class Collapses extends React.Component {
   render() {
     return (
@@ -188,6 +186,58 @@ function Collapses() {
 export default Collapses;
 `;
 
+const codeCustomMarkup = `import React from "react";
+// reactstrap components
+import { UncontrolledCollapse } from "reactstrap";
+// core components
+
+function Example(){
+  return (
+    <>
+      <div className="item">
+        <a
+          href="#pablo"
+          id="exampleAccordion1"
+          onClick={e => e.preventDefault()}
+        >
+          Toggle item
+        </a>
+        <UncontrolledCollapse
+          role="tabpanel"
+          toggler="#exampleAccordion1"
+          defaultOpen
+        >
+          <p className=" mb-3">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            pretium lorem non vestibulum scelerisque. Proin a vestibulum sem,
+            eget tristique massa. Aliquam lacinia rhoncus nibh quis ornare.
+          </p>
+        </UncontrolledCollapse>
+      </div>
+      <div className="item">
+        <a
+          href="#pablo"
+          id="exampleAccordion2"
+          onClick={e => e.preventDefault()}
+        >
+          Toggle item 2
+        </a>
+        <UncontrolledCollapse role="tabpanel" toggler="#exampleAccordion2">
+          <p className=" mb-3">
+            Donec at ipsum dignissim, rutrum turpis scelerisque, tristique
+            lectus. Pellentesque habitant morbi tristique senectus et netus et
+            malesuada fames ac turpis egestas. Vivamus nec dui turpis. Orci
+            varius natoque penatibus et magnis dis parturient montes, nascetur
+            ridiculus mus.
+          </p>
+        </UncontrolledCollapse>
+      </div>
+    </>
+  );
+}
+
+export default Example;`;
+
 function Collapses() {
   // collapse states and functions
   const [collapses, setCollapses] = React.useState([]);
@@ -346,6 +396,50 @@ function Collapses() {
       </div>
       <SyntaxHighlighter language="jsx" style={prism}>
         {codeAccordionExample}
+      </SyntaxHighlighter>
+      <p>You can also create accordions with custom markup.</p>
+      <div className="bd-example">
+        <div className="item">
+          <a
+            href="#pablo"
+            id="exampleAccordion1"
+            onClick={e => e.preventDefault()}
+          >
+            Toggle item
+          </a>
+          <UncontrolledCollapse
+            role="tabpanel"
+            toggler="#exampleAccordion1"
+            defaultOpen
+          >
+            <p className=" mb-3">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              pretium lorem non vestibulum scelerisque. Proin a vestibulum sem,
+              eget tristique massa. Aliquam lacinia rhoncus nibh quis ornare.
+            </p>
+          </UncontrolledCollapse>
+        </div>
+        <div className="item">
+          <a
+            href="#pablo"
+            id="exampleAccordion2"
+            onClick={e => e.preventDefault()}
+          >
+            Toggle item 2
+          </a>
+          <UncontrolledCollapse role="tabpanel" toggler="#exampleAccordion2">
+            <p className=" mb-3">
+              Donec at ipsum dignissim, rutrum turpis scelerisque, tristique
+              lectus. Pellentesque habitant morbi tristique senectus et netus et
+              malesuada fames ac turpis egestas. Vivamus nec dui turpis. Orci
+              varius natoque penatibus et magnis dis parturient montes, nascetur
+              ridiculus mus.
+            </p>
+          </UncontrolledCollapse>
+        </div>
+      </div>
+      <SyntaxHighlighter language="jsx" style={prism}>
+        {codeCustomMarkup}
       </SyntaxHighlighter>
       <h2>Prop</h2>
       <p>
