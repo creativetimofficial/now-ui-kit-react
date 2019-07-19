437fba9e9d6e11891c14a3c3be004ab5be1ebbb9
Manu <emanuelnazare@yahoo.ro>
2019-07-19 11:59:48 +0300

    - deleted some comments

--- src/views/docs/Sections/restyled-components/Dropdowns.js
+++ src/views/docs/Sections/restyled-components/Dropdowns.js
@@ -1,21 +1,3 @@
-/*!
-
-=========================================================
-* Paper Kit React - v1.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-kit-react
-
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 /*eslint-disable*/
 import React from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
