26cf597c6520792d5d032cf937910ddc28dc59aa
Manu <emanuelnazare@yahoo.ro>
2019-07-19 11:47:22 +0300

    - Dropwond docs

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import "assets/css/bootstrap.min.css";
 import "assets/scss/now-ui-kit.scss";
 import "assets/demo/demo.css";
 import "assets/demo/nucleo-icons-page-styles.css";
+import "views/docs/Documentation.css";
 // pages for this kit
 import Index from "views/Index.js";
 import NucleoIcons from "views/NucleoIcons.js";
