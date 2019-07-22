c91437c1313664004baea8edb8c5ccb49098d163
Manu <emanuelnazare@yahoo.ro>
2019-07-22 11:11:34 +0300

    - copyrights

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+/*
+
+=========================================================
+* Now UI Kit React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/now-ui-kit-react
+* Copyright 2019 Creative Tim (http://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/now-ui-kit-react/blob/master/LICENSE.md)
+
+* Designed by www.invisionapp.com Coded by www.creative-tim.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
