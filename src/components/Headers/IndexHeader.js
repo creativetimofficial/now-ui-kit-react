51903a3f3bf7fd10371d3f84568e0debda343612
EINazare <emanuelioannazare@gmail.com>
2021-05-19 09:52:41 +0300

    fix: solve new images require

--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -28,7 +28,7 @@ function IndexHeader() {
         <div
           className="page-header-image"
           style={{
-            backgroundImage: "url(" + require("assets/img/header.jpg") + ")",
+            backgroundImage: "url(" + require("assets/img/header.jpg").default + ")",
           }}
           ref={pageHeader}
         ></div>
@@ -37,7 +37,7 @@ function IndexHeader() {
             <img
               alt="..."
               className="n-logo"
-              src={require("assets/img/now-logo.png")}
+              src={require("assets/img/now-logo.png").default}
             ></img>
             <h1 className="h1-seo">Now UI Kit.</h1>
             <h3>A beautiful Bootstrap 4 UI kit. Yours free.</h3>
@@ -48,7 +48,7 @@ function IndexHeader() {
               <img
                 alt="..."
                 className="invision-logo"
-                src={require("assets/img/invision-white-slim.png")}
+                src={require("assets/img/invision-white-slim.png").default}
               ></img>
             </a>
             . Coded by{" "}
@@ -59,7 +59,7 @@ function IndexHeader() {
               <img
                 alt="..."
                 className="creative-tim-logo"
-                src={require("assets/img/creative-tim-white-slim2.png")}
+                src={require("assets/img/creative-tim-white-slim2.png").default}
               ></img>
             </a>
             .
