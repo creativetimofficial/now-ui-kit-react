1c7959d7f3f7ba10b7b47d3c30a0e4a35d1451fd
EINazare <emanuelioannazare@gmail.com>
2021-05-19 09:52:43 +0300

    fix: solve new images require

--- src/components/Headers/LandingPageHeader.js
+++ src/components/Headers/LandingPageHeader.js
@@ -27,7 +27,7 @@ function LandingPageHeader() {
         <div
           className="page-header-image"
           style={{
-            backgroundImage: "url(" + require("assets/img/bg6.jpg") + ")",
+            backgroundImage: "url(" + require("assets/img/bg6.jpg").default + ")",
           }}
           ref={pageHeader}
         ></div>
