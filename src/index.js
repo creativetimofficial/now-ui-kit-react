e94b773b068287f0872938b90bbbaf050f1654de
Maisam Afshar <maisam.afshar@gmail.com>
2023-05-15 13:33:17 +0330

    refactor: update to router v6

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@
 */
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 // styles for this kit
 import "assets/css/bootstrap.min.css";
@@ -35,28 +35,14 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
-    <Switch>
-      <Switch>
-        <Route path="/index" render={(props) => <Index {...props} />} />
-        <Route
-          path="/nucleo-icons"
-          render={(props) => <NucleoIcons {...props} />}
-        />
-        <Route
-          path="/landing-page"
-          render={(props) => <LandingPage {...props} />}
-        />
-        <Route
-          path="/profile-page"
-          render={(props) => <ProfilePage {...props} />}
-        />
-        <Route
-          path="/login-page"
-          render={(props) => <LoginPage {...props} />}
-        />
-        <Redirect to="/index" />
-        <Redirect from="/" to="/index" />
-      </Switch>
-    </Switch>
+    <Routes>
+      <Route path="/index" element={<Index />} />
+      <Route path="/nucleo-icons" element={<NucleoIcons />} />
+      <Route path="/landing-page" element={<LandingPage />} />
+      <Route path="/profile-page" element={<ProfilePage />} />
+      <Route path="/login-page" element={<LoginPage />} />
+
+      <Route path="*" element={<Navigate to="/index" replace />} />
+    </Routes>
   </BrowserRouter>
 );
